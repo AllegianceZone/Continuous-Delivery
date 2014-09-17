22bf6357788aa157afcda790b0e986959f1dc341
ImagoTrigger <imagotrigger@gmail.com>
2014-09-17 15:39:41 -0500

    start on the self updating

---
+++ self-update.bat
@@ -0,0 +1,5 @@
+cd C:\build\Continuous-Delivery
+git clean -fd
+git checkout -f
+git pull origin master
+git checkout HEAD
--- self-update.pl
+++ self-update.pl
@@ -1,12 +1,28 @@
+use strict;
+
+my $build = $ARGV[0];
+
+if (-e "C:\\self-updated") {
+	print "Build & Deploy tools were updated!\n";
+	exit 0;
+} else {
+	print "Build & Deploy tools getting updated first...\n";
+}
+
+my $cmd = "C:\\build\\self-update.bat";
+system($cmd);
+$cmd = "copy C:\\build\\Continuous-Delivery\\* C:\\build /Y";
+system($cmd);
+
 #TODO
-# - Make this the first step in the bitten recipe
-# - Check for a drop file indicating if tools have been upgraded (if not, continue below)
-# - pull https://github.com/AllegianceZone/Continuous-Delivery to C:\build\Continuous-Delivery
-# - replace everything from C:\build\Continuous-Delivery to C:\build (dont need to take subdirectories)
 # - ftp the contents of C:\build\Continuous-Delivery\az to allegiancezone.cloudapp.net:21122 /
 # - LWP our way to admin session and POST C:\build\Continuous-Delivery\trac\bitten.xml to http://trac.allegiancezone.com/admin/bitten/configs/Allegiance
-# - using same admin session from above invalidate our current build
-# - drop a file indicating tools have been updated
+# - using same admin session from above and invalidate our current build
+
+open(TOUCH,">C:\\self-updated");
+print "\n";
+close TOUCH;
+
+#TODO
 # - kill bitten-slave, it will automatically restart and skip the tool updating/bitten-slave killing step
-# - Make a second step in the bitten recipe that deletes our drop file indicating tools can be updated again
 # - Profit?
