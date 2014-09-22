85f18d803ddcdd743d78b8f0c67b854db03f1dad
ImagoTrigger <imagotrigger@gmail.com>
2014-09-22 17:44:23 -0500

    check if its up

--- shutdown.pl
+++ shutdown.pl
@@ -7,100 +7,59 @@ use Win32::OLE;
 
 my $host = "azbuildslave";
 
-print "Registering local AGC...\n";	
-my $cmd = "regsvr32 C:\\AllegBeta\\AGC.dll /s";
-`$cmd`;
-sleep(3);
-
-my $utl  = Win32::OLE->new("AllSrv.AdminSession");
-my $srv = $utl->Server;
-exit 0 if (!$srv);
-my $games = $srv->Games;
-
-my %stats;
-
-my $biggameid = -1; my $bigcnt = 0; my $hasships = 0;
- foreach my $value (in $games) {
-	 my $gameid = $value->GameID;
-	 $stats{$gameid}{name} = $value->Name;
-	my $teams = $value->Teams;
-	 foreach my $team (in $teams) {
-			my $stations = $team->Stations;
-			my $count =  $stations->Count;
-			$stats{$gameid}{numteams}++ if ($count);
-			$stats{$gameid}{numstations} += $count;
-	 }	 
-	 my $cnt = 0;
-	 my $ships = $value->Ships;
-	 foreach my $ship (in $ships) {
-		$cnt++;
-	 }
-	 if ($cnt > $bigcnt) {
-		 $biggameid = $value->GameID;
-		 $bigcnt = $cnt;
-		 $hasships = 1;
-	 }
-
+open (PS,"C:\\build\\pslist.exe 2>crap AllSrv |");
+my $pid = 0;
+while (<PS>) {
+	if ($_ =~ /AllSrv\s+(\d+)/) {
+		$pid = $1;
+	}
 }
+close PS;
 
-if ($hasships && $stats{$biggameid}) {
-	my $stations = $stats{$biggameid}{numstations};
-	if (!$stations) {
-		print "Players are connected but idle...\n";
-		$srv->SendMsg("Shutting down in one minute.");
-		sleep(30);
-		$srv->SendMsg("Shutting down in 30 seconds.");
-		sleep(25);		
-		$srv->SendMsg("Shutting down in 5 seconds.");
-		sleep(2);	
-		$srv->SendMsg("Shutting down in 3 seconds.");
-		sleep(1);				
-		$srv->SendMsg("Shutting down in 2 seconds.");
-		sleep(1);				
-		$srv->SendMsg("Shutting down in 1 seconds.");
-		sleep(1);
-		$utl->Stop;
-	} else {
-		if ($stats{$biggameid}{numteams} > 1) {
-			my $game = $stats{$biggameid}{name};
-			$game .= ' Game' if ($game !~ / game$/i);
-			$game = 'The '.$game if ($game !~ /^the /i);
-			print "Players are in a $game...\n";
-			$srv->SendMsg("Shutting down after $game is over.");
-			my $i = 0;
-			while(GetStations($biggameid) != 0) {
-				sleep(5);
-				$i++;
-				if ($i >= 2160) {
-					$srv->SendMsg("Aborting shutdown, epic game!");
-					print "Aborting!\n";
-					exit 1;
-				}
-				if ($i % 84 == 0) {
-					print "Still waiting for $game to finish...\n";
-					$srv->SendMsg("Shutting down after $game is over.");
-				}
-			}
-			print "Finished\n";
-			$srv->SendMsg("Shutting down in four minutes.");
-			sleep(120);					
-			$srv->SendMsg("Shutting down in two minutes.");
-			sleep(90);
-			$srv->SendMsg("Shutting down in 30 seconds.");
-			sleep(25);		
-			$srv->SendMsg("Shutting down in 5 seconds.");
-			sleep(2);	
-			$srv->SendMsg("Shutting down in 3 seconds.");
-			sleep(1);				
-			$srv->SendMsg("Shutting down in 2 seconds.");
-			sleep(1);				
-			$srv->SendMsg("Shutting down in 1 seconds.");
-			sleep(1);
-			$utl->Stop;					
-		} else {
-			print "Player(s) are by themselves, give them a few...\n";
-			$srv->SendMsg("Shutting down in two minutes.");
-			sleep(90);					
+if ($pid != 0) {
+
+	print "Registering local AGC...\n";	
+	my $cmd = "regsvr32 C:\\AllegBeta\\AGC.dll /s";
+	`$cmd`;
+	sleep(3);
+
+	my $utl  = Win32::OLE->new("AllSrv.AdminSession");
+	my $srv = $utl->Server;
+	exit 0 if (!$srv);
+	my $games = $srv->Games;
+
+	my %stats;
+
+	my $biggameid = -1; my $bigcnt = 0; my $hasships = 0;
+	 foreach my $value (in $games) {
+		 my $gameid = $value->GameID;
+		 $stats{$gameid}{name} = $value->Name;
+		my $teams = $value->Teams;
+		 foreach my $team (in $teams) {
+				my $stations = $team->Stations;
+				my $count =  $stations->Count;
+				$stats{$gameid}{numteams}++ if ($count);
+				$stats{$gameid}{numstations} += $count;
+		 }	 
+		 my $cnt = 0;
+		 my $ships = $value->Ships;
+		 foreach my $ship (in $ships) {
+			$cnt++;
+		 }
+		 if ($cnt > $bigcnt) {
+			 $biggameid = $value->GameID;
+			 $bigcnt = $cnt;
+			 $hasships = 1;
+		 }
+
+	}
+
+	if ($hasships && $stats{$biggameid}) {
+		my $stations = $stats{$biggameid}{numstations};
+		if (!$stations) {
+			print "Players are connected but idle...\n";
+			$srv->SendMsg("Shutting down in one minute.");
+			sleep(30);
 			$srv->SendMsg("Shutting down in 30 seconds.");
 			sleep(25);		
 			$srv->SendMsg("Shutting down in 5 seconds.");
@@ -112,13 +71,66 @@ if ($hasships && $stats{$biggameid}) {
 			$srv->SendMsg("Shutting down in 1 seconds.");
 			sleep(1);
 			$utl->Stop;
+		} else {
+			if ($stats{$biggameid}{numteams} > 1) {
+				my $game = $stats{$biggameid}{name};
+				$game .= ' Game' if ($game !~ / game$/i);
+				$game = 'The '.$game if ($game !~ /^the /i);
+				print "Players are in a $game...\n";
+				$srv->SendMsg("Shutting down after $game is over.");
+				my $i = 0;
+				while(GetStations($biggameid) != 0) {
+					sleep(5);
+					$i++;
+					if ($i >= 2160) {
+						$srv->SendMsg("Aborting shutdown, epic game!");
+						print "Aborting!\n";
+						exit 1;
+					}
+					if ($i % 84 == 0) {
+						print "Still waiting for $game to finish...\n";
+						$srv->SendMsg("Shutting down after $game is over.");
+					}
+				}
+				print "Finished\n";
+				$srv->SendMsg("Shutting down in four minutes.");
+				sleep(120);					
+				$srv->SendMsg("Shutting down in two minutes.");
+				sleep(90);
+				$srv->SendMsg("Shutting down in 30 seconds.");
+				sleep(25);		
+				$srv->SendMsg("Shutting down in 5 seconds.");
+				sleep(2);	
+				$srv->SendMsg("Shutting down in 3 seconds.");
+				sleep(1);				
+				$srv->SendMsg("Shutting down in 2 seconds.");
+				sleep(1);				
+				$srv->SendMsg("Shutting down in 1 seconds.");
+				sleep(1);
+				$utl->Stop;					
+			} else {
+				print "Player(s) are by themselves, give them a few...\n";
+				$srv->SendMsg("Shutting down in two minutes.");
+				sleep(90);					
+				$srv->SendMsg("Shutting down in 30 seconds.");
+				sleep(25);		
+				$srv->SendMsg("Shutting down in 5 seconds.");
+				sleep(2);	
+				$srv->SendMsg("Shutting down in 3 seconds.");
+				sleep(1);				
+				$srv->SendMsg("Shutting down in 2 seconds.");
+				sleep(1);				
+				$srv->SendMsg("Shutting down in 1 seconds.");
+				sleep(1);
+				$utl->Stop;
+			}
 		}
+	} else {
+		$utl->Stop;
 	}
-} else {
-	$utl->Stop;
-}
 
-print "OK to kill AllSrv\n";	
+	print "OK to kill AllSrv\n";	
+}
 exit(0);
 
 
