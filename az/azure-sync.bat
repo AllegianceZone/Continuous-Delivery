087f3639e25bbf9e8769f2d2c106b8dbee135cff
ImagoTrigger <imagotrigger@gmail.com>
2014-09-21 20:54:56 -0500

    put /XO back on the AZCopy for the install container

--- az/azure-sync.bat
+++ az/azure-sync.bat
@@ -3,5 +3,5 @@ C:
 cd C:\deploy
 set /p key=<key.txt
 AzCopy C:\deploy\autoupdate https://azcdn.blob.core.windows.net/autoupdate /destkey:%key% /S /XO /Y
-AzCopy C:\deploy\install https://azcdn.blob.core.windows.net/install /destkey:%key% /S /Y
+AzCopy C:\deploy\install https://azcdn.blob.core.windows.net/install /destkey:%key% /S /XO /Y
 AzCopy C:\deploy\config https://azcdn.blob.core.windows.net/config /destkey:%key% /S /XO /Y
