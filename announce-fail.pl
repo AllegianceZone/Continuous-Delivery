9b7f4ac44c2aadfd041433ec240b8cb92ffbc21a
ImagoTrigger <imagotrigger@gmail.com>
2015-06-02 03:16:54 -0500

    Update announce-fail.pl

--- announce-fail.pl
+++ announce-fail.pl
@@ -7,7 +7,7 @@ use AnyEvent::JSONRPC::Lite::Client;
 my $b = $ARGV[0];
 my $c = $ARGV[1];
 
-my $msg = "b$b oh noes FAILED compiling $c project! See http://trac.allegiancezone.com/build/Allegiance/$b for details";
+my $msg = "b$b oh noes FAILED compiling $c project! See http://trac.spacetechnology.net/build/Allegiance/$b for details";
 
 my $client = AnyEvent::JSONRPC::Lite::Client->new( host => 'azforum.cloudapp.net',port => 49153);
 my $res = $client->call( echo => $msg )->recv;
