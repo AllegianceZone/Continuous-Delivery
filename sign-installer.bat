f07e5274ffaec145aaf062440c6e962e77baf0fa
ImagoTrigger <imagotrigger@gmail.com>
2015-06-03 02:54:45 -0500

    Update sign-installer.bat

--- sign-installer.bat
+++ sign-installer.bat
@@ -1,6 +1,15 @@
 rem #Imago <imagotrigger@gmail.com> Signs the build's installer
 
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+if not defined DevEnvDir (
+	if defined VS100COMNTOOLS (
+		call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+	)
+)
+if not defined DevEnvDir (
+	if defined VS120COMNTOOLS (
+		call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+	)
+)
 
-signtool sign /v /s Mine /n "Allegiance Zone Development" /d "Allegiance 1.%2 Installer" /du "http://www.allegiancezone.com" /t "http://timestamp.verisign.com/scripts/timstamp.dll" C:\build\Package\%1
+signtool sign /v /s Mine /n "Allegiance Zone Development" /d "Allegiance 1.%2 Installer" /du "http://www.allegiancezone.com" /t "http://timestamp.verisign.com/scripts/timstamp.dll" %3\Package\%1
