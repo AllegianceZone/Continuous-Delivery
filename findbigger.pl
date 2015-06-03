a7d52626651bfc9cb0b54fc5048e01be0f784e66
ImagoTrigger <imagotrigger@gmail.com>
2015-06-03 02:42:24 -0500

    Update findbigger.pl

--- findbigger.pl
+++ findbigger.pl
@@ -1,16 +1,18 @@
 use strict;
 
-opendir(DIR,"C:\\build\\AutoUpdate\\Game\\");
+my $azbp = $ARGV[0];
+
+opendir(DIR,"$azbp\\AutoUpdate\\Game\\");
 my @files = readdir DIR;
 closedir DIR;
 
-open(LIST,"C:\\build\\betalist.txt");
+open(LIST,"$azbp\\betalist.txt");
 my @lines = <LIST>;
 close LIST;
 
 foreach my $file (@files) {
 	next if $file =~ /^\./;
-	my $size = (stat("C:\\build\\Autoupdate\\Game\\$file"))[7];
+	my $size = (stat("$azbp\\Autoupdate\\Game\\$file"))[7];
 	foreach my $line (@lines) {
 		my ($listsize,$listfile) = (split(/\s/,$line))[2,4];
 		if ($listfile eq $file) {
