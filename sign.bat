a099e58d92eeac0995821f19a00b8f19e23ac0a6
ImagoTrigger <imagotrigger@gmail.com>
2015-06-03 02:54:05 -0500

    Update sign.bat

--- sign.bat
+++ sign.bat
@@ -1,12 +1,21 @@
 rem #Imago <imagotrigger@gmail.com> Signs the build's objects
 
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+if not defined DevEnvDir (
+	if defined VS100COMNTOOLS (
+		call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+	)
+)
+if not defined DevEnvDir (
+	if defined VS120COMNTOOLS (
+		call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+	)
+)
 
-signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll C:\build\Allegiance\objs10\%1\AllSrvUI\AllSrvUI.exe
-signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll C:\build\Allegiance\objs10\%1\FedSrv\Allsrv.exe
-signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll C:\build\Allegiance\objs10\%1\Reloader\Reloader.exe
-signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll C:\build\Allegiance\objs10\%1\AutoUpdate\AutoUpdate.exe
-signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll C:\build\Allegiance\objs10\%1\Lobby\AllLobby.exe
-signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll C:\build\Allegiance\objs10\%1\WinTrek\Allegiance.exe
-signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll C:\build\Allegiance\objs10\%1\AGC\AGC.dll
+signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll %2\Allegiance\%3\%1\AllSrvUI\AllSrvUI.exe
+signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll %2\Allegiance\%3\%1\FedSrv\Allsrv.exe
+signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll %2\Allegiance\%3\%1\Reloader\Reloader.exe
+signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll %2\Allegiance\%3\%1\AutoUpdate\AutoUpdate.exe
+signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll %2\Allegiance\%3\%1\Lobby\AllLobby.exe
+signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll %2\Allegiance\%3\%1\WinTrek\Allegiance.exe
+signtool sign -a -v -s Mine -n "Allegiance Zone Development" -t http://timestamp.verisign.com/scripts/timestamp.dll %2\Allegiance\%3\%1\AGC\AGC.dll
