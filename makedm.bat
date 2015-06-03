9d5bc4bb45a9034ba5f1803555dfac771ff9dedf
ImagoTrigger <imagotrigger@gmail.com>
2015-06-03 03:54:22 -0500

    Update makedm.bat

--- makedm.bat
+++ makedm.bat
@@ -1,6 +1,5 @@
 @echo off
-C:
 copy %1\Allegiance\src\Pigs\PigAccts\PigAccts.ini C:\AllegBeta\PigAccts.ini /Y
-cd %1\Allegiance\src\Pigs\Scripts
+cd /D %1\Allegiance\src\Pigs\Scripts
 perl CreatePigs.pl dm
 rem perl CreatePig.pl 1
