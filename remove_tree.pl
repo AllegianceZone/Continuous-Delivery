086abc138f0c076ba6b4b3740380d29033d63a19
ImagoTrigger <imagotrigger@gmail.com>
2015-06-03 02:44:55 -0500

    Update remove_tree.pl

--- remove_tree.pl
+++ remove_tree.pl
@@ -1,5 +1,5 @@
 use File::Path qw(remove_tree);
 use Data::Dumper;
-
-remove_tree("C:\\build\\Test\\PCore005b", {verbose => 1,error => \my $err_list,});
+my $azbp = $ARGV[0];
+remove_tree("$azbp\\Test\\PCore005b", {verbose => 1,error => \my $err_list,});
 print Dumper($err_list);
